65772ab9b95a8ae9a8d9c8ef80e7f3718354e775
Gustavo <gustavolopez819@gmail.com>
2026-04-02 20:46:51 -0500

    ADDED crete business function

--- src/lib/services/businessService.ts
+++ src/lib/services/businessService.ts
@@ -1,3 +1,4 @@
+import { BusinessForm } from "@/types";
 import { getSupabaseBrowserClient } from "../supabase/browser-client";
 
 const supabase = getSupabaseBrowserClient();
@@ -7,6 +8,18 @@ export async function getBusinessByUserId(id: string) {
     .from("businesses")
     .select("*")
     .eq("owner_id", id);
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function createBusiness(business: BusinessForm) {
+  const { data, error } = await supabase
+    .from("businesses")
+    .insert(business)
+    .select()
+    .single()
 
   if (error) throw error;
 
