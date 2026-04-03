d8765124b35697a7ea74cfe5d046d0e692669841
Gustavo <gustavolopez819@gmail.com>
2026-04-02 20:20:45 -0500

    CREATED business Type & updated supabase type

---
+++ src/types/Business.ts
@@ -0,0 +1,15 @@
+export interface BusinessForm {
+  name: string;
+  plan: string;
+  description: string;
+  legal_name: string;
+  rfc: string;
+  phone: string;
+  email: string;
+  country: string;
+  state: string;
+  city: string;
+  address: string;
+  zip_code: string;
+  owner_id: string;
+}
