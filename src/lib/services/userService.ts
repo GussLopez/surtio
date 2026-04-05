a1cc99b66dc5d3a0049b83793e07e0ba15fa64c3
Gustavo <gustavolopez819@gmail.com>
2026-04-04 19:48:52 -0500

    REFACT user functions

--- src/lib/services/userService.ts
+++ src/lib/services/userService.ts
@@ -4,23 +4,40 @@ const supabase = getSupabaseBrowserClient();
 
 export async function getUsers(businessId: string) {
   const { data, error } = await supabase
-    .from("profiles")
-    .select("*")
-    .order("created_at")
-    .eq("business_id", businessId);
-
+    .from("memberships")
+    .select(
+      `
+      role,
+      created_at,
+      profiles!memberships_profile_fk (*)
+    `,
+    )
+    .eq("business_id", businessId)
+    .order("created_at");
   if (error) throw error;
 
   return data;
 }
 
-export async function updateProfile(profileId: string, employe: { full_name: string; role: string }) {
-  const { error } = await supabase
-    .from('profiles')
-    .update(employe)
-    .eq("id", profileId)
+export async function updateEmployee(
+  profileId: string,
+  businessId: string,
+  data: { full_name: string; role: string }
+) {
+  const { error: profileError } = await supabase
+    .from("profiles")
+    .update({ full_name: data.full_name })
+    .eq("id", profileId);
 
-  if (error) throw error;
+  if (profileError) throw profileError;
+
+  const { error: membershipError } = await supabase
+    .from("memberships")
+    .update({ role: data.role })
+    .eq("user_id", profileId)
+    .eq("business_id", businessId);
+
+  if (membershipError) throw membershipError;
 }
 
 export async function deleteProfile(profileId: string) {
@@ -28,7 +45,7 @@ export async function deleteProfile(profileId: string) {
     .from("profiles")
     .delete()
     .eq("id", profileId);
-    
+
   if (error) throw error;
 }
 
@@ -37,9 +54,40 @@ export async function getProfileById(id: string) {
     .from("profiles")
     .select("*")
     .eq("id", id)
-    .single()
+    .single();
 
   if (error) throw error;
 
   return data;
+}
+
+export async function getMyRole(businessId: string) {
+  const user = await supabase.auth.getUser();
+
+  const { data, error } = await supabase
+    .from("memberships")
+    .select("role")
+    .eq("business_id", businessId)
+    .eq("user_id", user.data.user?.id!)
+    .single();
+
+  if (error) throw error;
+
+  return data.role;
+}
+
+async function waitForProfile(userId: string) {
+  for (let i = 0; i < 5; i++) {
+    const { data } = await supabase
+      .from("profiles")
+      .select("id")
+      .eq("id", userId)
+      .single();
+
+    if (data) return true;
+
+    await new Promise((res) => setTimeout(res, 200));
+  }
+
+  throw new Error("Profile no se creó a tiempo");
 }
