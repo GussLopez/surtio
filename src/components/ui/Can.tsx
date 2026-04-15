ff7e2b2aaa010ce75463c5165e66541ad70279e6
Gustavo <gustavolopez819@gmail.com>
2026-04-14 20:39:38 -0500

    CREATED Can component for role security

---
+++ src/components/ui/Can.tsx
@@ -0,0 +1,16 @@
+import { getMyRole } from "@/lib/services/userService";
+import { useBusinessStore } from "@/store/BusinessStore";
+import { useEffect, useState } from "react";
+
+export default function Can({ roles, children }: { roles: string[], children: React.ReactNode }) {
+  const businessId = useBusinessStore(state => state.id)
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    getMyRole(businessId!).then(setRole);
+  }, []);
+
+  if (!role) return null;
+
+  return roles.includes(role) ? <>{children}</> : null;
+}
