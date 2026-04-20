f48634c956ca51968f9b777abcb7d9dca1154015
Gustavo <gustavolopez819@gmail.com>
2026-04-20 12:52:55 -0500

    CHANGED session management to use getSession instead of getUser and improved token handling

--- src/components/auth/SessionListener.tsx
+++ src/components/auth/SessionListener.tsx
@@ -1,18 +1,23 @@
 'use client'
 
 import { getProfileById } from "@/lib/services/userService";
-import { getSupabaseBrowserClient } from "@/lib/supabase/browser-client"
+import { getSupabaseBrowserClient } from "@/lib/supabase/browser-client";
+import { useAuthStore } from "@/store/AuthStore";
 import { useUserStore } from "@/store/UserStore";
 import { useEffect } from "react";
 
 export default function SessionListener() {
   const supabase = getSupabaseBrowserClient();
+
   const setUser = useUserStore((state) => state.setUser);
   const clearUser = useUserStore((state) => state.clearUser);
+
+  const setToken = useAuthStore((state) => state.setToken);
 
   useEffect(() => {
     const fetchProfile = async (id: string) => {
       const profile = await getProfileById(id);
+
       if (profile) {
         setUser({
           id: profile.id,
@@ -21,29 +26,45 @@ export default function SessionListener() {
           role: profile.memberships[0].role
         });
       }
-    }
+    };
 
     const initSession = async () => {
-      const { data } = await supabase.auth.getUser();
+      const { data } = await supabase.auth.getSession();
 
-      if (data.user?.id) {
-        await fetchProfile(data.user.id);
+      const session = data.session;
+
+      if (session?.access_token) {
+        setToken(session.access_token);
+      } else {
+        setToken(null);
       }
-    }
+
+      if (session?.user?.id) {
+        await fetchProfile(session.user.id);
+      }
+    };
+
     initSession();
 
-    const { data: { subscription } } = supabase.auth.onAuthStateChange(
-      async (_event, session) => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(async (_event, session) => {
 
-        if (session?.user.id) {
-          await fetchProfile(session.user.id);
-        } else {
-          clearUser();
-        }
-      });
+      if (session?.access_token) {
+        setToken(session.access_token);
+      } else {
+        setToken(null);
+      }
+
+      if (session?.user?.id) {
+        await fetchProfile(session.user.id);
+      } else {
+        clearUser();
+      }
+    });
 
     return () => subscription.unsubscribe();
-  }, [])
+  }, []);
 
   return null;
 }
