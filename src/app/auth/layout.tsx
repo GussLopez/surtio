ffdf63a269d068dba1b862ce8eb06e175e2dcec0
Gustavo <gustavolopez819@gmail.com>
2026-05-02 01:15:50 -0500

    ADDED grid dashed background

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,29 +10,82 @@ export default function AuthLayout({
     <div className="w-full lg:grid lg:grid-cols-2 min-h-screen">
       {children}
       <div className="hidden lg:block w-full p-5">
-        <div className="w-full h-full flex flex-col gap-10 px-20 justify-center rounded-2xl bg-primary/85 relative">
-          <div className="">
-            <h2 className="max-w-120 text-3xl font-medium text-white">Gestiona tu tienda y tus ventas sin esfuerzo.</h2>
-            <p className="max-w-130 mt-5 text-sm text-white/90">Inicia sesión para acceder al dashboard de tu gestor de inventario y controla tu negocio.</p>
+        <div className="w-full h-full flex flex-col gap-10 px-20 justify-center rounded-2xl bg-primary/85 relative overflow-hidden">
+          {/* Dashed Bottom Fade Grid */}
+          <div
+            className="absolute inset-0 z-0 opacity-30"
+            style={{
+              backgroundImage: `
+        linear-gradient(to right, #e7e5e4 1px, transparent 1px),
+        linear-gradient(to bottom, #e7e5e4 1px, transparent 1px)
+      `,
+              backgroundSize: "20px 20px",
+              backgroundPosition: "0 0, 0 0",
+              maskImage: `
+         repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            radial-gradient(ellipse 100% 80% at 50% 100%, #000 50%, transparent 90%)
+      `,
+              WebkitMaskImage: `
+  repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            radial-gradient(ellipse 100% 80% at 50% 100%, #000 50%, transparent 90%)
+      `,
+              maskComposite: "intersect",
+              WebkitMaskComposite: "source-in",
+            }}
+          />
+
+          <div className="z-10">
+            <h2 className="max-w-120 text-4xl font-medium text-white">Gestiona tu tienda y tus ventas sin esfuerzo.</h2>
+            <p className="max-w-120 mt-3 text-sm text-white/80">Inicia sesión para acceder al dashboard de tu gestor de inventario y controla tu negocio.</p>
           </div>
-          <div className="relative w-full">
-            <Skeleton className="w-full h-90 flex justify-center items-center  bottom-28 left-20 rounded-xl text-muted-foreground bg-muted">
-              <Image size={30} />
-            </Skeleton>
+          <div className="relative w-full z-10">
+            <img
+              src={'/img/login/login-preview.png'}
+              className="w-full h-90 flex justify-center items-center bottom-28 left-20 rounded-sm text-muted-foreground bg-muted dark:hidden"
+            />
+            <img
+              src={'/img/login/login-dark-preview.png'}
+              className="w-full h-90 hidden justify-center items-center bottom-28 left-20 rounded-sm text-muted-foreground bg-muted dark:flex"
+            />
             <img
               src={'/img/login/login-chart.png'}
               alt="Demo de Gráfica"
-              className="w-52 h-auto absolute -right-8 top-8 block dark:hidden"
+              className="w-52 h-auto absolute -right-8 -top-12 block dark:hidden shadow-xl rounded-md"
             />
             <img
               src={'/img/login/login-chart-dark.png'}
               alt="Demo de Gráfica"
-              className="w-52 h-auto absolute -right-8 top-8 hidden dark:block"
+              className="w-52 h-auto absolute -right-8 -top-12 hidden dark:block shadow-lg shadow-muted-foreground/20 rounded-md"
             />
           </div>
         </div>
       </div>
-
     </div>
   );
 }
