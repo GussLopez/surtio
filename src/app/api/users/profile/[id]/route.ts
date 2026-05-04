9dc12c195002640edca06355c22652aa73139b5a
Gustavo <gustavolopez819@gmail.com>
2026-05-04 11:54:36 -0500

    CREATED get User by id route

--- src/app/api/users/profile/[id]/route.ts
+++ src/app/api/users/profile/[id]/route.ts
@@ -34,8 +34,56 @@ export async function PUT(
     if (error) {
       return Response.json({ error: error.message }, { status: 500 });
     }
-    
+
     return Response.json({ success: true });
+  } catch (err: any) {
+    return Response.json(
+      { error: err.message || "Unexpected error" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const { id } = await params;
+
+    if (!id) {
+      return Response.json({ error: "Id inválido" }, { status: 500 });
+    }
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll() {},
+        },
+      },
+    );
+
+    const { data, error } = await supabase
+      .from("profiles")
+      .select(
+        `
+        *,
+        memberships (role)
+      `,
+      )
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      return Response.json({ error: error.message }, { status: 500 });
+    }
+
+    return Response.json(data);
   } catch (err: any) {
     return Response.json(
       { error: err.message || "Unexpected error" },
