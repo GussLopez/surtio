c6391a061fa8931fb076adc0e941f0efe6a4b51d
Gustavo <gustavolopez819@gmail.com>
2026-05-04 11:54:58 -0500

    ADDED GET and POST routes for sales with error handling and cookie management

--- src/app/api/sales/[id]/route.ts
+++ src/app/api/sales/[id]/route.ts
@@ -35,8 +35,109 @@ export async function DELETE(
     return Response.json({ success: true });
   } catch (err: any) {
     return Response.json(
-      { error: err.message || 'Unexpected error' },
-      { status: 500 }
-    )
+      { error: err.message || "Unexpected error" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const { id } = await params;
+    if (!id) {
+      return Response.json({ error: "Id inválido" }, { status: 500 });
+    }
+
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll() {},
+        },
+      },
+    );
+
+    const { data, error } = await supabase
+      .from("sales")
+      .select(
+        `*,
+      sale_items (
+        id,
+        quantity,
+        price,
+        product_id,
+        products (
+          id,
+          name,
+          sku,
+          cost
+        )
+      )
+        `,
+      )
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      return Response.json({ error: error.message }, { status: 500 });
+    }
+    return Response.json(data);
+  } catch (err: any) {
+    return Response.json(
+      { error: err.message || "Unexpected error" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const { id } = await params;
+    const { cancelReason } = await req.json();
+
+    if (!id) {
+      return Response.json({ error: "Id inválido" }, { status: 500 });
+    }
+
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll() {},
+        },
+      },
+    );
+
+    const { error } = await supabase.rpc("cancel_sale", {
+      p_sale_id: id,
+      p_cancel_reason: cancelReason,
+    });
+
+    if (error) {
+      return Response.json({ error: error.message }, { status: 500 });
+    }
+
+    return Response.json({ success: true });
+  } catch (err: any) {
+    return Response.json(
+      { error: err.message || "Unexpected Error" },
+      { status: 500 },
+    );
   }
 }
