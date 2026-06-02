b689a61f8b8cc6c5826f527bb65bd5b098afbe6d
Gustavo <gustavolopez819@gmail.com>
2026-06-02 13:56:59 -0500

    feat(stock-drawer): add patch api route to update stock

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -80,7 +80,55 @@ export async function PUT(
       .update(product)
       .eq("id", productId)
       .select();
+    console.error(error);
+    if (error) {
+      return Response.json({ error: error.message }, { status: 500 });
+    }
+    if (!data || data.length === 0) {
+      return Response.json(
+        { error: "No tienes permisos para editar este producto" },
+        { status: 403 },
+      );
+    }
+    return Response.json({ success: true });
+  } catch (err: any) {
+    return Response.json(
+      { error: err.message || "Unexpected error" },
+      { status: 500 },
+    );
+  }
+}
 
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const { id: productId } = await params;
+    const { stock } = await req.json();
+    if (!productId) {
+      return Response.json({ error: "Id inválido" }, { status: 500 });
+    }
+
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll() {},
+        },
+      },
+    );
+    const { data, error } = await supabase
+      .from("products")
+      .update({ stock })
+      .eq("id", productId)
+      .select();
+    console.error(error);
     if (error) {
       return Response.json({ error: error.message }, { status: 500 });
     }
