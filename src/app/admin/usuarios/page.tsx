53173e2890f39f330b937bf81f824cf3019d3404
Gustavo <gustavolopez819@gmail.com>
2026-06-06 10:48:03 -0500

    feat(loading-state): add loading cart skeleton

--- src/app/admin/usuarios/page.tsx
+++ src/app/admin/usuarios/page.tsx
@@ -77,7 +77,7 @@ export default function UsuariosPage() {
       {error && (
         <ServerError />
       )}
-      {!isLoading && (
+      {isLoading && (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-10">
           <UserCardSkeleton />
           <UserCardSkeleton />
