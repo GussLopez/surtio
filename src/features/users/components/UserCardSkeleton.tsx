fca3c1c21101046d59e45aa687baaea607b1ab40
Gustavo <gustavolopez819@gmail.com>
2026-06-05 23:55:51 -0500

    feat(user): add ui to skeleton component

--- src/features/users/components/UserCardSkeleton.tsx
+++ src/features/users/components/UserCardSkeleton.tsx
@@ -1,8 +1,17 @@
+import { Skeleton } from "@/shared/components/ui/skeleton";
+
 export default function UserCardSkeleton() {
 
   return (
     <div>
-
+      <div className={`h-24 w-full bg-linear-to-br relative}`}>
+        <div className="absolute top-4 right-4">
+          <Skeleton className="h-4 w-20 rounded-full" />
+        </div>
+        <div className="absolute -bottom-10 left-4">
+          <Skeleton className="h-20 w-20 flex justify-center items-center rounded-full" />
+        </div>
+      </div>
     </div>
   )
 }
