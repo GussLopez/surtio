bdc2ef3be1be43031a97d38f7af2d8c09439cf22
Gustavo <gustavolopez819@gmail.com>
2026-06-05 23:58:46 -0500

    feat(loading): add loading state

--- src/features/users/components/UserCardSkeleton.tsx
+++ src/features/users/components/UserCardSkeleton.tsx
@@ -3,15 +3,20 @@ import { Skeleton } from "@/shared/components/ui/skeleton";
 export default function UserCardSkeleton() {
 
   return (
-    <div>
-      <div className={`h-24 w-full bg-linear-to-br relative}`}>
+    <div className="bg-red-50">
+      <div className={`h-24 w-full bg-linear-to-br relative`}>
         <div className="absolute top-4 right-4">
           <Skeleton className="h-4 w-20 rounded-full" />
         </div>
         <div className="absolute -bottom-10 left-4">
           <Skeleton className="h-20 w-20 flex justify-center items-center rounded-full" />
         </div>
+        <div>
+          <Skeleton className="w-1/2 h-5" />
+          <Skeleton className="w-25 h-4" />
+        </div>
       </div>
+
     </div>
   )
 }
