1a5023834d7bef7269568ee776017389151141f2
Gustavo <gustavolopez819@gmail.com>
2026-06-06 10:46:23 -0500

    feat(loading-state): add loading cart skeleton

--- src/features/users/components/UserCardSkeleton.tsx
+++ src/features/users/components/UserCardSkeleton.tsx
@@ -3,20 +3,25 @@ import { Skeleton } from "@/shared/components/ui/skeleton";
 export default function UserCardSkeleton() {
 
   return (
-    <div className="bg-red-50">
+    <div className="">
       <div className={`h-24 w-full bg-linear-to-br relative`}>
         <div className="absolute top-4 right-4">
-          <Skeleton className="h-4 w-20 rounded-full" />
+          <Skeleton className="h-4 w-17 rounded-full" />
         </div>
         <div className="absolute -bottom-10 left-4">
           <Skeleton className="h-20 w-20 flex justify-center items-center rounded-full" />
         </div>
-        <div>
-          <Skeleton className="w-1/2 h-5" />
-          <Skeleton className="w-25 h-4" />
-        </div>
       </div>
 
+      <div className="mt-10 space-y-1 p-4">
+        <Skeleton className="w-1/2 h-5" />
+        <Skeleton className="w-3/4 h-3" />
+      </div>
+
+      <div className="flex gap-3 p-4 mt-5">
+        <Skeleton className="w-4/5 h-10" />
+        <Skeleton className="w-1/5 h-10" />
+      </div>
     </div>
   )
 }
