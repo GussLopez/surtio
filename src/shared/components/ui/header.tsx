d07588e80d482f0fa0c47323fa0728914e2ed38a
Gustavo <gustavolopez819@gmail.com>
2026-07-11 23:42:06 -0500

    feat(header): add mobile header view

--- src/shared/components/ui/header.tsx
+++ src/shared/components/ui/header.tsx
@@ -1,4 +1,4 @@
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Hamburger, Menu } from "lucide-react";
 import { Button } from "./button";
 import Link from "next/link";
 
@@ -10,44 +10,61 @@ export default function Header() {
 
   ]
   return (
-    <header className="sticky top-0 z-100 hidden w-full border-b border-input lg:block bg-background">
-      <div className="h-16">
-        <nav className="relative w-full h-full flex items-center justify-between flex-1 max-w-7xl mx-auto px-6">
+    <header>
+      <div className="sticky top-0 z-100 block w-full border-b border-input lg:hidden bg-[#FEFDFB]">
+        <div className="h-16 px-6 flex justify-between items-center">
           <div className="flex items-center gap-1.5">
             <span>
               <ArrowUpRight />
             </span>
             <span className="text-xl font-semibold">Surtio</span>
           </div>
+          <div className="">
+            <button className="p-2 border border-input rounded-md">
+              <Menu className="size-6"/>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="sticky top-0 z-100 hidden w-full border-b border-input lg:block bg-background">
+        <div className="h-16">
+          <nav className="relative w-full h-full flex items-center justify-between flex-1 max-w-7xl mx-auto px-6">
+            <div className="flex items-center gap-1.5">
+              <span>
+                <ArrowUpRight />
+              </span>
+              <span className="text-xl font-semibold">Surtio</span>
+            </div>
 
-          <div className="absolute translate-x-1/2 right-1/2 h-full flex items-center gap-5">
-            {links.map((link, i) => (
-              <Link
-                key={i}
-                href={link.path}
-                className="px-2.5 py-2 rounded-lg text-sm font-medium hover:bg-accent"
+            <div className="absolute translate-x-1/2 right-1/2 h-full flex items-center gap-5">
+              {links.map((link, i) => (
+                <Link
+                  key={i}
+                  href={link.path}
+                  className="px-2.5 py-2 rounded-lg text-sm font-medium hover:bg-accent"
+                >
+                  {link.text}
+                </Link>
+              ))}
+            </div>
+
+            <div className="flex gap-2">
+              <Button
+                className="rounded-md"
+                variant={'outline'}
+                asChild
               >
-                {link.text}
-              </Link>
-            ))}
-          </div>
-
-          <div className="flex gap-2">
-            <Button
-              className="rounded-md"
-              variant={'outline'}
-              asChild
-            >
-              <Link href={'/auth/login'}>Inicia sesión</Link>
-            </Button>
-            <Button
-              className="rounded-md bg-foreground text-background dark:hover:text-white"
-              asChild
-            >
-              <Link href={'/admin'}>Demostración</Link>
-            </Button>
-          </div>
-        </nav>
+                <Link href={'/auth/login'}>Inicia sesión</Link>
+              </Button>
+              <Button
+                className="rounded-md bg-foreground text-background dark:hover:text-white"
+                asChild
+              >
+                <Link href={'/admin'}>Demostración</Link>
+              </Button>
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   )
