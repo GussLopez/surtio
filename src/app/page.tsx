531be7716c98bbc87343b7058b50dbc86df8a25e
Gustavo <gustavolopez819@gmail.com>
2026-07-19 20:52:13 -0500

    feat(ui): add StepOne component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/shared/components/ui/header";
 import Hero from "../features/landing/components/Hero";
+import StepOne from "@/features/landing/components/StepOne";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+
+      <StepOne />
     </main>
 
   );
