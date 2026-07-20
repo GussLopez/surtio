ac09f06f31fc695d5c1f8202c3a4adeee478336b
Gustavo <gustavolopez819@gmail.com>
2026-07-20 12:34:48 -0500

    feat(ui): add footer and page metadata title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import Header from "@/shared/components/ui/header";
 import Hero from "../features/landing/components/Hero";
 import StepOne from "@/features/landing/components/StepOne";
+import Footer from "@/shared/components/ui/footer";
+import { Metadata } from "next";
+import { GeneratePageTitle } from "@/shared/utils/metadata";
+
+export const metadata: Metadata = {
+  title: GeneratePageTitle('La plataforma para hacer crecer tu negocio')
+}
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header />
-      <Hero />
-
-      <StepOne />
-    </main>
+      <main>
+        <Hero />
+        <div className="border-y border-input/60">
+          <StepOne />
+        </div>
+      </main>
+      <Footer />
+    </>
 
   );
 }
