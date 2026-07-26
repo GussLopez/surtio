32c4f5bc1995a135840558ae873a874c0748f8af
Gustavo <gustavolopez819@gmail.com>
2026-07-25 23:14:23 -0500

    feat(search-product): refactor to use Combobox for improved product selection UI

--- src/features/sales/components/SearchProductInput.tsx
+++ src/features/sales/components/SearchProductInput.tsx
@@ -1,32 +1,16 @@
-'use client'
+'use client';
 import { useState } from 'react';
 
-import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react'
-
-import { Button } from '@/shared/components/ui/button'
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList
-} from '@/shared/components/ui/command'
-import { Popover, PopoverContent, PopoverTrigger } from '@/shared/components/ui/popover'
-
-import { cn } from '@/shared/utils/utils'
 import { useQuery } from '@tanstack/react-query'
-import { ProductItem } from '@/shared/types'
+import { Product, ProductItem } from '@/shared/types'
 import { useDebounce } from 'use-debounce';
 import { useBusinessStore } from '@/shared/store/BusinessStore';
+import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxInput, ComboboxItem, ComboboxList } from '@/shared/components/ui/combobox';
 
 interface SearchProductProps {
   setProduct: (product: ProductItem) => void;
-  btnSize?: 'xs' | 'sm' | 'lg' | 'default';
-  btnClass: string;
-  placeholder?: string;
 }
-const SearchProductInput = ({ setProduct, btnClass, btnSize = 'default', placeholder }: SearchProductProps) => {
+const SearchProductInput = ({ setProduct }: SearchProductProps) => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState('');
   const [inputValue, setInputValue] = useState('');
@@ -53,73 +37,48 @@ const SearchProductInput = ({ setProduct, btnClass, btnSize = 'default', placeho
   const selectedProduct = products?.find(p => p.id === value)
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant='outline'
-          size={btnSize}
-          role='combobox'
-          aria-expanded={open}
-          className={btnClass}
-          aria-label='Framework combobox'
-        >
-          Buscar por nombre o SKU...
-          <ChevronsUpDownIcon className='opacity-50' />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent align='start' className='p-0 w-110'>
-        <Command shouldFilter={false}>
-          <CommandInput
-            placeholder='Buscar por nombre o SKU...'
-            className='h-9'
-            value={inputValue}
-            onValueChange={setInputValue}
-          />
-          <CommandList>
-            {isLoading && (
-              <CommandEmpty>Buscando...</CommandEmpty>
-            )}
-            {!isLoading && products === null && (
-              <CommandEmpty >Escribe al menos 1 caracter para buscar...</CommandEmpty>
-            )}
-            {!isLoading && products?.length === 0 && (
-              <CommandEmpty>Producto no encontrado.</CommandEmpty>
-            )}
-            <CommandGroup>
-              {products?.map(product => (
-                <CommandItem
-                  key={product.id}
-                  value={product.id}
-                  onSelect={currentValue => {
-                    setProduct({
-                      ...product,
-                      quantity: 1
-                    })
-                    setValue(currentValue === value ? '' : currentValue)
-                    setOpen(false)
-                    setInputValue('')
-                  }}
-                >
-                  <div className='w-full flex items-center justify-between'>
-                    <div className='flex items-center gap-3'>
-                      <div className='w-10 h-10 border border-input rounded-lg'></div>
-                      <div className='text-sm'>
-                        <p className='font-medium text-neutral-700 dark:text-neutral-200'>{product.name} - ({product.sku})</p>
-                        <span className='text-muted-foreground'>$ {product.price}</span>
-                      </div>
-                    </div>
-                    <div className='px-2 py-1 rounded-full bg-green-200'>
-                      <span className='text-xs'>{product.stock} {product.unit}s</span>
-                    </div>
+    <Combobox items={products || []}>
+      <ComboboxInput
+        value={value}
+        placeholder="Selecciona un producto"
+        className="bg-background"
+      />
+      <ComboboxContent>
+        <ComboboxEmpty>No se encontraron productos.</ComboboxEmpty>
+        <ComboboxList>
+          {(product: Product) => (
+            <ComboboxItem
+              key={product.id}
+              value={product}
+              onClick={() => {
+                setProduct({
+                  ...product,
+                  quantity: 1
+                })
+                setOpen(false)
+                setInputValue('')
+              }}
+            >
+              <div className='w-full flex items-center justify-between'>
+                <div className='flex items-center gap-3'>
+                  <div className='w-10 h-10 border border-input rounded-lg'></div>
+                  <div className='text-sm'>
+                    <p className='font-medium text-neutral-700 dark:text-neutral-200'>
+                      {product.name} - ({product.sku})
+                    </p>
+                    <span className='text-muted-foreground'>$ {product.price}</span>
                   </div>
+                </div>
+                <div className='px-2 rounded-full bg-green-100 text-green-700'>
+                  <span className='text-xs font-medium'>{product.stock} {product.unit}s</span>
+                </div>
+              </div>
 
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+            </ComboboxItem>
+          )}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox >
   )
 }
 
